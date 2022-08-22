2eae1386978d2a9d96055934fb8457d45d2f9321
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-08-22 20:50:44 +0000

    A function to create a node

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef BINARY_TREES
 #define BINARY_TREES
 
+/* HEADER FILES */
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -17,6 +20,10 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
+
+/* FUNCTION PROTOTYPES */
+void binary_tree_print(const binary_tree_t *tree);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 
 #endif /*BINARY_TREES*/
