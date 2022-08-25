bb3c5fe139dd678d5f04b38d465e9ca3d58bc662
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-08-25 01:48:40 +0000

    Test

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+
+/**
+ * num_node - Calculates the number of nodes
+ * @tree: Pointer to binary tree
+ * @ptr: Pointer to number of nodes
+ *
+ * Return: Always 0 (Succes)
+ *
+ */
+int num_node(const binary_tree_t *tree, size_t *ptr)
+{
+	if (tree->left != NULL || tree->right != NULL)
+		(*ptr)++;
+	if (tree->left != NULL && tree->right != NULL)
+		num_node(tree->left, ptr);
+	else if (tree->left == NULL && tree->right != NULL)
+		num_node(tree->right, ptr);
+	else if (tree->left != NULL && tree->right == NULL)
+		num_node(tree->left, ptr);
+	return (0);
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: Height of tree
+ *         0 - if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height = 0;
+
+	if (tree == NULL)
+		return (0);
+	num_node(tree, &height);
+	return (height);
+}
