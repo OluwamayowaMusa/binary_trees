5c191db57ce72fed486a6185d750bdd5961485ee
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-08-25 08:37:09 +0000

    Wrote a function to return the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	if (node->parent->parent != NULL)
+	if (node->parent->parent->right != NULL)
 		return ((node->parent->n == node->parent->parent->right->n) ?
 			node->parent->parent->left : node->parent->parent->right);
 	return (NULL);
